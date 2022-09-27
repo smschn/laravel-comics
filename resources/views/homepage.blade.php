68d310416fbf784e04d64783f10351468cf90c0f
Luca Somaschini <somas99@gmail.com>
2022-09-27 17:47:58 +0200

    Test main

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,14 +10,14 @@
     prova
     prova
     prova
-{{--     <div class="containerWidth">
+    <div class="containerWidth">
         <div class="card_comics">
             <div class="img_container">
                 <img v-bind:src="details.thumb" v-bind:alt="details.type">
             </div>
-            <h6>{{details.series.toUpperCase()}}</h6>
+            <h6>{{-- {{details.series.toUpperCase()}} --}}</h6>
         </div>
     </div>
-    <div class="button">LOAD MORE</div> --}}
+    <div class="button">LOAD MORE</div>
 </main>
 @endsection
