e1467c6ef49e1da1bec8251a70c01809095c01d5
Luca Somaschini <somas99@gmail.com>
2022-09-27 16:03:04 +0200

    Basic structure, added img folder

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Comics</title>
+</head>
+<body>
+
+    @include('partials.header')
+
+    <main>
+        main
+    </main>
+
+    @include('partials.footer')
+
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <div>
+        <img src="{{asset('images/dc-logo.png')}}" alt="Logo">
+    </div>
+    <nav>
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </nav>
+    <hr>
+</header>
