26a392572c59acb4911d3da6d3bfddbeb3f9cf6a
Luca Somaschini <somas99@gmail.com>
2022-09-27 16:55:48 +0200

    Added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,67 @@
 <footer>
-    <hr>
-    footer
+    <div class="f_top">
+        <div class="ft_container containerWidth">
+            <div class="ft_menu">
+                <nav>
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Tv</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </nav>
+                <nav>
+                    <h4>DC</h4>
+                    <ul>
+                        <li><a href="#">Terms of use</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                        <li><a href="#">Ad choices</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent workshop</a></li>
+                        <li><a href="#">CPSC certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop help</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                        <li><a href="#">Contact us</a></li>
+                   </ul>
+                </nav>
+                <nav>
+                    <h4>SITES</h4>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD magazine</a></li>
+                        <li><a href="#">DC kids</a></li>
+                        <li><a href="#">DC universe</a></li>
+                        <li><a href="#">DC powervisa</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="ft_logobg">
+                <img src="{{asset('images/dc-logo-bg.png')}}" alt="Logo_bg">
+            </div>
+        </div>
+    </div>
+    <div class="f_bottom">
+        <div class="fb_container containerWidth">
+            <button>SIGN UP NOW!</button>
+            <ul>
+                <li>FOLLOW US</li>
+                <li><img src="{{asset('images/footer-facebook.png')}}" alt="facebook"></li>
+                <li><img src="{{asset('images/footer-periscope.png')}}" alt="periscope"></li>
+                <li><img src="{{asset('images/footer-pinterest.png')}}" alt="pinterest"></li>
+                <li><img src="{{asset('images/footer-twitter.png')}}" alt="twitter"></li>
+                <li><img src="{{asset('images/footer-youtube.png')}}" alt="youtube"></li>
+            </ul>
+        </div>
+    </div>
 </footer>
